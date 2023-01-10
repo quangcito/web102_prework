34b15ce275f0b25314894dec6c2e9f07e1e40b99
Quang Duong Nguyen <quanguyendg@gmail.com>
2023-01-09 19:17:08 -0600

    challenges finished

--- index.js
+++ index.js
@@ -182,7 +182,16 @@ const sortedGames =  GAMES_JSON.sort( (item1, item2) => {
 });
 
 // use destructuring and the spread operator to grab the first and second games
+let [firstGame, secondGame, ...otherGames] = sortedGames;
 
 // create a new element to hold the name of the top pledge game, then append it to the correct element
+const topFundedGame = document.createElement("p");
+let {name} = firstGame;
+topFundedGame.innerHTML = `${name}`;
+firstGameContainer.append(topFundedGame);
 
 // do the same for the runner up item
+// let {name} = secondGame;
+const runnerUpFundedGame = document.createElement("p");
+runnerUpFundedGame.innerHTML = `${secondGame.name}`;
+secondGameContainer.append(runnerUpFundedGame);
